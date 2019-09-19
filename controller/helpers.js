9ed66127b24dd1f29421331bc8099216290dab33
Brarian <b.namonywa@gmail.com>
2019-09-18 22:25:33 -0500

    validators for username and password added to helpers

---
+++ controller/helpers.js
@@ -0,0 +1,22 @@
+const validateUsername = usernameData => {
+  return new Promise((resolve, reject) => {
+    if (username < 6) {
+      reject('username needs to be at least 6 characters');
+    } else {
+      resolve();
+    }
+  });
+};
+
+const validatePassword = passwordData => {
+  return new Promise((resolve, reject) => {
+    if (
+      passwordData.length < 8 ||
+      !passwordData.includes('characters') ||
+      !passwordData.includes('number')
+    ) {
+      reject('password does not meet the criteria');
+    }
+    resolve();
+  });
+};
