4257d205c0f59c2827e874b1154b3d77213ea435
Brarian <b.namonywa@gmail.com>
2019-09-24 23:50:04 -0500

    register user started

--- cognito.js
+++ cognito.js
@@ -5,3 +5,26 @@ import {
   AuthenticationDetails,
   CognitoUserAttribute
 } from 'amazon-cognito-identity-js';
+import * as AWSCONFIGS from './aws_config';
+
+exports.registerUser = (req, res) => {
+  const { name } = req.body;
+  const { email } = req.body;
+  if (req.body.password !== req.body.passwordCheck) {
+    console.log(error);
+    res.send({ error });
+  }
+  const poolData = {
+    userPoolId: AWSCONFIGS.default.USER_POOL_ID,
+    clientId: AWSCONFIGS.default.USER_POOL_ID
+  };
+
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+  let attributeName = AmazonCognitoIdentity.CognitoUserAttribute(name);
+  let attributeEmail = AmazonCognitoIdentity.CognitoUserAttribute(email);
+
+  const attributeList = [attributeName, attributeEmail];
+
+  let cognitoUSer = re;
+};
